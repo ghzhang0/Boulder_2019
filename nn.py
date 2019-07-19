674341ae6049ef5e26e57a373c1a99f7eed68ad9
Veronika Golovanova <golovanova.v.b@gmail.com>
2019-07-19 16:51:42 -0500

    Add comet tracking of NN performance

--- nn.py
+++ nn.py
@@ -17,6 +17,7 @@ parser.add_argument("--inputSize", nargs='*', type=int, required=True)
 parser.add_argument("--hiddenSize", nargs='*', type=float, required=True)
 args = parser.parse_args()
 
+# Define comet experiment
 experiment = Experiment("S1gz8UBL6fpAmuPjMIP4zGXMt", project_name="boulder")
 
 learning_rate = 20
@@ -24,6 +25,7 @@ learning_rate = 20
 def f(x): # nonlinear conversion function to binary
     return x.sigmoid()
 
+# Track performance for comet
 with experiment.train():
     for index_n1 in range(len(args.inputSize)):
         N = args.inputSize[index_n1] # input dimension and number of training behaviors
@@ -49,16 +51,16 @@ with experiment.train():
             list_loss = []
 
             for t in range(args.epochs):
-            # Forward pass: compute predicted y using operations on Tensors. Since w1 and
-            # w2 have requires_grad=True, operations involving these Tensors will cause
-            # PyTorch to build a computational graph, allowing automatic computation of
-            # gradients. Since we are no longer implementing the backward pass by hand we
-            # don't need to keep references to intermediate values.
+                # Forward pass: compute predicted y using operations on Tensors. Since w1 and
+                # w2 have requires_grad=True, operations involving these Tensors will cause
+                # PyTorch to build a computational graph, allowing automatic computation of
+                # gradients. Since we are no longer implementing the backward pass by hand we
+                # don't need to keep references to intermediate values.
                 y_pred = f(f(x.mm(w1).add(b1)).mm(w2).add(b2))
                 y_prednp = y_pred.detach().numpy()
                 list_y_pred.append(y_prednp)
-            # Compute and print loss. Loss is a Tensor of shape (), and loss.item()
-            # is a Python number giving its value.
+                # Compute and print loss. Loss is a Tensor of shape (), and loss.item()
+                # is a Python number giving its value.
                 loss = (y_pred - y).pow(2).mean()
                 list_loss.append(loss.item())
 
@@ -68,23 +70,23 @@ with experiment.train():
                     np.savetxt('bias1_' + str(args.inputSize[index_n1]) + '_' + str(args.hiddenSize[index_n2]) + '.dat', b1.detach().numpy())
                     np.savetxt('bias2_' + str(args.inputSize[index_n1]) + '_' + str(args.hiddenSize[index_n2]) + '.dat', b2.detach().numpy())
             
-            # Use autograd to compute the backward pass. This call will compute the
-            # gradient of loss with respect to all Tensors with requires_grad=True.
-            # After this call w1.grad and w2.grad will be Tensors holding the gradient
-            # of the loss with respect to w1 and w2 respectively.
+                # Use autograd to compute the backward pass. This call will compute the
+                # gradient of loss with respect to all Tensors with requires_grad=True.
+                # After this call w1.grad and w2.grad will be Tensors holding the gradient
+                # of the loss with respect to w1 and w2 respectively.
                 loss.backward()
 
-            # Update weights using gradient descent. For this step we just want to mutate
-            # the values of w1 and w2 in-place; we don't want to build up a computational
-            # graph for the update steps, so we use the torch.no_grad() context manager
-            # to prevent PyTorch from building a computational graph for the updates
+                # Update weights using gradient descent. For this step we just want to mutate
+                # the values of w1 and w2 in-place; we don't want to build up a computational
+                # graph for the update steps, so we use the torch.no_grad() context manager
+                # to prevent PyTorch from building a computational graph for the updates
                 with torch.no_grad():
                     w1 -= learning_rate * w1.grad
                     w2 -= learning_rate * w2.grad
                     b1 -= learning_rate * b1.grad
                     b2 -= learning_rate * b2.grad
 
-                # Manually zero the gradients after running the backward pass
+                    # Manually zero the gradients after running the backward pass
                     w1.grad.zero_()
                     w2.grad.zero_()
                     b1.grad.zero_()
