ffa57d6ae7656e62dad7f2d43936a9223f0884df
Grace H. Zhang <ghzhang>
2019-12-10 10:53:10 -0500

    Add SGD notebook, script, analysis file

---
+++ nn_sgd.py
@@ -0,0 +1,124 @@
+#Code in file autograd/two_layer_net_autograd.py
+#python nn.py --it_ind 0 --inputSize 100 --hiddenSize 100 --learningRate $GHZHANG17_TASK_ID --epochs 100000000
+from comet_ml import Experiment
+
+import torch
+import copy
+import argparse
+import numpy as np
+from numpy import genfromtxt
+import difflib
+
+device = torch.device('cpu')
+#device = torch.device('cuda') # Uncomment this to run on GPU
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--it_ind",type=int, required=True)
+parser.add_argument("--epochs",type=int, required=True)
+parser.add_argument("--inputSize", nargs='*', type=int, required=True)
+parser.add_argument("--hiddenSize", nargs='*', type=float, required=True)
+parser.add_argument("--learningRate", nargs='*', type=float, required=True)
+parser.add_argument("--outputSize", nargs='*', type=int, required=False)
+args = parser.parse_args()
+
+# Define comet experiment
+experiment = Experiment(api_key="n8etP46kIWiChy4sbrEWSuqCG",
+                        project_name="boulder", workspace="ghzhang0")
+
+learning_rate = args.learningRate[0]
+print(learning_rate)
+it_ind = args.it_ind
+
+# Define nonlinear activation function
+def f(x): # nonlinear conversion function to binary
+    return x.sigmoid()
+
+# Track performance for comet
+with experiment.train():
+    for index_n1 in range(len(args.inputSize)):
+        for index_n3 in range(len(args.outputSize)):
+            N = args.inputSize[index_n1] # input dimension and number of training behaviors
+            M = N # args.outputSize[index_n3] # output dimension
+            x = torch.tensor(np.genfromtxt("x_{}.csv".format(it_ind), delimiter=','), device=device).float()
+            y = torch.tensor(np.genfromtxt("y_{}.csv".format(it_ind), delimiter=','), device=device).float()
+            for index_n2 in range(len(args.hiddenSize)):
+                R = int((args.hiddenSize[index_n2])) # hidden dimension
+                model = torch.nn.Sequential(
+                          torch.nn.Linear(N, R),
+                          torch.nn.Sigmoid(),
+                          torch.nn.Linear(R,M),
+                          torch.nn.Sigmoid())
+                loss_fn = torch.nn.MSELoss()
+                optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+
+                list_behavior = []
+                list_loss = []
+            
+                for t in range(args.epochs):
+                    # Run the forward pass
+                    y_pred = model(x)
+
+                    # Calculate the loss
+                    loss = loss_fn(y_pred, y)
+                    list_loss.append(loss.item())
+
+                    # Zero the gradients before running the backward pass.
+                    optimizer.zero_grad()
+
+                    # Backward pass
+                    loss.backward()
+
+                    # Update weights using SGD
+                    optimizer.step()
+                        
+                    #Criterion for stopping according to number of learnt behaviours
+                    behavior = 0
+                    threshold = 0.5
+                    per = 0.99
+                    y_pred_binary = np.abs(np.round(y_pred.data.numpy()+0.5-threshold))
+                    for j in range(len(y)):
+                        s = difflib.SequenceMatcher(None, y.data.numpy()[j],y_pred_binary[j])
+                        if s.ratio() > per:
+                            behavior += 1
+                    list_behavior.append(behavior)
+                    # Log to Comet.ml
+                    experiment.log_metrics({"loss":loss.item(), "behavior":behavior}, step=t)            
+                    if behavior == N:
+                        # Extract parameters
+                        trained_parameters = []
+                        for param in model.named_parameters():
+                            trained_parameters.append(param[1].data.numpy())
+
+                        w1 = trained_parameters[0]
+                        b1 = trained_parameters[1]
+                        w2 = trained_parameters[2]
+                        b2 = trained_parameters[3]
+                        # Save parameters
+                        np.savetxt('weights1_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2])  + '.dat', w1)
+                        np.savetxt('weights2_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2])  +'.dat', w2)
+                        np.savetxt('bias1_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2])  +'.dat', b1)
+                        np.savetxt('bias2_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2])  +'.dat', b2)
+                        break
+                
+                    # print results when max epoch is reached
+                    if t == args.epochs - 1: 
+                        # Extract parameters
+                        trained_parameters = []
+                        for param in model.named_parameters():
+                            trained_parameters.append(param[1].data.numpy())
+
+                        w1 = trained_parameters[0]
+                        b1 = trained_parameters[1]
+                        w2 = trained_parameters[2]
+                        b2 = trained_parameters[3]
+                        np.savetxt('weights1_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2])  +'.dat', w1)
+                        np.savetxt('weights2_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2])  +'.dat', w2)
+                        np.savetxt('bias1_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2])  +'.dat', b1)
+                        np.savetxt('bias2_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2])  +'.dat', b2)
+
+                #list_y_pred = np.array(list_y_pred)
+                list_loss = np.array(list_loss)
+                list_behavior = np.array(list_behavior)
+                #np.savetxt('y_pred_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2]) + '.dat', np.array(list_y_pred[-500:]).flatten().reshape(500*N, N))
+                np.savetxt('loss_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2])  +'.dat', list_loss[-500:])
+                np.savetxt('behavior_' + str(learning_rate) + '_' + str(args.hiddenSize[index_n2])  +'.dat', list_behavior[-500:])
