3d61b35f6375a517ae0afc17587d43a90dcd0779
Riccardo Ravasio <ravasio@Riccardos-MacBook-Pro.local>
2020-11-19 12:18:36 +0100

    Added noise=1 simulations

--- Check_othersNM_modularity/nn_modularity.sh
+++ Check_othersNM_modularity/nn_modularity.sh
@@ -3,13 +3,13 @@
 work_dir=/Users/ravasio/Documents/Boulder_2019/Check_othersNM_modularity
 cd $work_dir
 
-declare -a hidden_=(2 5 7)
+declare -a hidden_=(15 30 45 75 150 225 300)
 declare -a arr=(0)
 
 for i in ${hidden_[@]}
 do
   for j in ${arr[@]}
   do
-    python nn_modularity.py --it_ind $j --inputSize 50 --outputSize 50 --k_ind 5 --noise 0 --hiddenSize $i --learningRate 5 --epochs 400000
+    python nn_modularity.py --it_ind $j --inputSize 300 --outputSize 300 --k_ind 30 --noise 1 --hiddenSize $i --learningRate 5 --epochs 400000
   done
 done
